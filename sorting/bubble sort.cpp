f08989a16fbd1a6bcd802bbbbb6d67b50c2682bd
MamunAmin <mamun.amin1994@gmail.com>
2021-04-25 04:03:29 +0600

    changed

--- sorting/bubble sort.cpp
+++ sorting/bubble sort.cpp
@@ -1,28 +1,29 @@
    //Task: An unsorted array is given.
-  //       We have to sort it using Selection Sort.
+  //       We have to sort it using Bubble Sort.
  //Complexity: O(n^2). In each iterate, it sorts the
-//             left most unsorted item.
+//             right most unsorted item.
+
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-int selectionSort(int ar[], int ar_size) {
-    for(int i=0; i < ar_size-1; i++) {
-        for(int j=i+1; j < ar_size; j++) {
-            if(ar[i] > ar[j])
-                swap(ar[i], ar[j]);
+int bubbleSort(int ar[], int ar_size) {
+    for(int i=1; i < ar_size; i++) {
+        for(int j=0; j < ar_size-i; j++) {
+            if(ar[j] > ar[j+1])
+                swap(ar[j], ar[j+1]);
         }
     }
 }
 
 //initialize unsorted array
-int arr[] = {14, 33, 12, 20, 25, 11};
+int arr[] = {53, 14, 33, 12, 20, 25, 11};
 
 int main() {
     int array_size = sizeof(arr)/4;
 
-    selectionSort(arr, array_size);
+    bubbleSort(arr, array_size);
 
     //print sorted array
     for(int i=0; i < array_size; i++) {
---
+++ sorting/counting sort.cpp
@@ -0,0 +1,36 @@
+   //Task: An unsorted array is given.
+  //       We have to sort it using Counting Sort.
+ //Complexity: O(n+W), where W is the value of array's max item.
+//             Good to use when dataset is big enough and W < 10^5.
+
+#include <bits/stdc++.h>
+#define MAX_VALUE_OF_ARRAY 53
+
+using namespace std;
+
+int countingSort(int ar[], int sorted_ar[], int ar_size) {
+    for(int i=0; i < ar_size; i++) {
+        sorted_ar[ar[i]]++;
+    }
+}
+
+//initialize unsorted array
+int arr[] = {53, 14, 33, 12, 11, 14, 20, 25, 11};
+int sorted_arr[MAX_VALUE_OF_ARRAY+1];
+
+int main() {
+    int array_size = sizeof(arr)/4;
+
+    memset(sorted_arr, 0, sizeof(sorted_arr));
+
+    countingSort(arr, sorted_arr, array_size);
+
+    //print sorted array
+    for(int i=0; i <= MAX_VALUE_OF_ARRAY; i++) {
+        while(sorted_arr[i]) {
+            printf("%d ", i);
+            sorted_arr[i]--;
+        }
+    }
+    return 0;
+}
