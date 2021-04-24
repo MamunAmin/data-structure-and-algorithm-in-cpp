86a95acfe63a96e9cb6d5c4dea5657ba26372b0e
MamunAmin <mamun.amin1994@gmail.com>
2021-04-25 02:51:39 +0600

    insertion sort added

---
+++ sorting/insertion sort.cpp
@@ -0,0 +1,37 @@
+   //Task: An unsorted array is given.
+  //       We have to sort it using Insertion Sort.
+ //Complexity: O(n^2). In each iterate, it sorts the
+//             left most segment.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int insertionSort(int ar[], int ar_size) {
+    for(int i=1; i < ar_size; i++) {
+        int currentValue = ar[i];
+        int j = i-1;
+        //sort the segment on left of index i
+        while( j>=0 && ar[j] > currentValue ) {
+            ar[j+1] = ar[j];
+            j--;
+        }
+        ar[j+1] = currentValue;
+    }
+}
+
+//initialize unsorted array
+int arr[] = {14, 33, 12, 20, 25, 11};
+
+int main() {
+    int array_size = sizeof(arr)/4;
+
+    insertionSort(arr, array_size);
+
+    //print sorted array
+    for(int i=0; i < array_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
